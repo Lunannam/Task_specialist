3e6dec85f5a9d5f16bda6357f7b69cbbe78e2070
Lunannam <lunkova02@gmail.com>
2022-07-24 23:00:09 +0300

    add task and code for the task

--- program.cs/Program.cs
+++ program.cs/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* написать программу, которая из имеющегося массива строк формирует массив строк,
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести к клавиатуры,
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+лучше обойтись исключительно массивами.
+ПРИМЕР:
+["hello", "2", "world", ":-)"] -> ["2",":-)"]
+
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+
+["Russia", "Denmark", "Kazan"] -> []*/
+
+
+string[] str = { "1234", "1567", "-2", "computer science"};
+Console.WriteLine("Исходный массив строк: ");
+for (int i = 0; i < str.Length; i++)
+Console.Write(str[i] + " ");
+Console.WriteLine("\n");
+
+string[] str2 = new string[str.Length];
+
+void SortFillNewString(string[] str, string[] str2)
+{
+    int count = 0;
+    bool flag = false;
+    for (int i = 0; i < str.Length; i++)
+    {
+    if(str[i].Length <= 3)
+        {
+        str2[count] = str[i];
+        count++;
+        flag = true;
+    }
+    }
+    if (flag == false)
+    {
+    Console.WriteLine("В исходном массиве не обнаружены подходящие строки!");
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Новый массив строк (длина строки<=3): ");
+SortFillNewString(str, str2);
+PrintArray(str2);
+
